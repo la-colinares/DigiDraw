2667e2ec01c375784afbc86f1c4ba6afb402206e
LA Colinares <dev.lacolinares@gmail.com>
2023-01-03 02:47:06 +0800

    display high score in main screen

--- app/src/main/java/com/lacolinares/digidraw/ui/pages/MainScreen.kt
+++ app/src/main/java/com/lacolinares/digidraw/ui/pages/MainScreen.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lacolinares.digidraw.R
@@ -18,17 +22,28 @@ import com.lacolinares.digidraw.ui.components.DigiSpace
 import com.lacolinares.digidraw.ui.components.DigiText
 import com.lacolinares.digidraw.ui.destinations.DigiModalDestination
 import com.lacolinares.digidraw.ui.destinations.QuizScreenDestination
+import com.lacolinares.digidraw.ui.pages.history.HistoryDataStore
 import com.lacolinares.digidraw.ui.theme.MineralGreen
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
+internal class MainScreenPreviewParam: PreviewParameterProvider<DestinationsNavigator>{
+    override val values: Sequence<DestinationsNavigator>
+        get() = sequenceOf(EmptyDestinationsNavigator)
+}
+
+@Preview(showSystemUi = true)
 @Destination(start = true)
 @Composable
 fun MainScreen(
-    navigator: DestinationsNavigator,
+    @PreviewParameter(MainScreenPreviewParam::class) navigator: DestinationsNavigator,
 ) {
     val buttonWidth = 120
     val context = LocalContext.current
+    val historyDataStore = HistoryDataStore(context)
+    val highestScore = historyDataStore.highestScore.collectAsState(initial = 0)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,6 +55,13 @@ fun MainScreen(
             text = stringResource(id = R.string.app_name),
             fontSize = 48.sp
         )
+        if (highestScore.value > 0){
+            DigiSpace(height = 40)
+            DigiText(
+                text = stringResource(id = R.string.high_score_text, highestScore.value),
+                fontSize = 18.sp
+            )
+        }
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
